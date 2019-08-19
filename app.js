ddd163b87c204c2b506da88ea4dfe68eaf7bef4f
Nguyen, Duc <vanducld@gmail.com>
2019-08-19 22:16:00 +0700

    Add linter and editorconfig

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-// [START gae_node_request_example]
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
@@ -27,12 +8,12 @@ app.get('/', (req, res) => {
     .end();
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
 
 module.exports = app;
